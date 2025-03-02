3c2b610a65ab69460e0f3be77cc7f142d69a45c1
titrn <146559025+titrn@users.noreply.github.com>
2025-03-02 16:26:19 -0500

    updated CORS

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"], # Change "*" to specific origins later for security reasons
+    allow_origins=["*"], # Change "*" to specific origins later for security reasons
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
